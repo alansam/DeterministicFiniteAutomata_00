082bfde855405b70f0db50011ce3ce1ee966ae62
Alan Sampson <alansam@users.noreply.github.com>
2022-12-28 02:41:00 +0000

    Code fix-ups and corrections.

--- main.c
+++ main.c
@@ -20,67 +20,113 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <ctype.h>
 
 #define ARRAYSIZE(a)	(sizeof(a) / sizeof(a[0]))
 
-enum states { EVEN, ODD };
+enum states { EVEN, ODD, };
 
-int delta(int state, int c);
-ssize_t set_search(int *set, size_t size, int v);
+int delta(int state, int ch);
+ssize_t set_search(int *set, size_t size, int vh);
 
-int main(void)
-{
-	int c, state;
+/*
+ *  MARK: main()
+ */
+int main(int argc, char const * argv[]) {
+	int ch, state;
+  int RC = EXIT_SUCCESS;
+
   [[maybe_unused]]
-	int Q[] = { EVEN, ODD };	/* set of states */
-	int F[] = { ODD };	/* set of accept states */
-	int sigma[] = { '0', '1' };	/* alphabet */
+	int QS[] = { EVEN, ODD, };	/* set of states */
+	int FS[] = { ODD, };	/* set of accept states */
+	int sigma[] = { '0', '1', };	/* alphabet */
 
-	state = EVEN;
-	while ((c = getchar()) != EOF) {
-		if (!isdigit(c) || set_search(sigma, ARRAYSIZE(sigma), c) == -1) {
+  state = EVEN;
+	while ((ch = getchar()) != EOF) {
+    if (isspace(ch)) {
+      printf("%12s: isspace(%#04x) ...\n",
+             __func__, ch);
+      break;
+    }
+
+    printf("%12s: isdigit(%#04x): %-5s\n",
+           __func__, ch, (isdigit(ch) ? "true" : "false"));
+
+    if (!isdigit(ch)
+    ||  set_search(sigma, ARRAYSIZE(sigma), ch) == -1) {
+      printf("%12s: GRONK!\n", __func__);
 			state = EVEN;
 			break;
 		}
-		state = delta(state, c);
+
+    state = delta(state, ch);
+    putchar('\n');
 	}
 
-	if (set_search(F, ARRAYSIZE(F), state) == -1) {
-		fprintf(stderr, "Invalid string\n");
-		return 1;
+  putchar('\n');
+
+  if (set_search(FS, ARRAYSIZE(FS), state) == -1) {
+		fprintf(stderr, "%12s: Invalid string\n", __func__);
+		RC = EXIT_FAILURE;
 	}
-	return 0;
+
+  printf("%12s: state[f] = %4s, RC = %d\n",
+         __func__, (state == ODD ? "odd" : "even"), RC);
+
+  return RC;
 }
 
-int delta(int state, int c)
-{
+/*
+ *  MARK: delta()
+ */
+int delta(int state, int ch) {
+  printf("%12s: state[i] = %4s, c = %#04x\n",
+         __func__, (state == ODD ? "odd" : "even"), ch);
+
 	switch (state) {
 	case EVEN:
-		if (c == '0')
-			state = EVEN;
-		else if (c == '1')
-			state = ODD;
+		if (ch == '0')      { state = EVEN; }
+		else if (ch == '1') { state = ODD; }
 		break;
-	case ODD:
-		if (c == '0')
-			state = ODD;
-		else if (c == '1')
-			state = EVEN;
+
+  case ODD:
+		if (ch == '0')      { state = ODD; }
+		else if (ch == '1') { state = EVEN; }
 		break;
+
 	default:
 		break;
 	}
-	return state;
+
+  printf("%12s: state[o] = %4s\n",
+         __func__, (state == ODD ? "odd" : "even"));
+
+  return state;
 }
 
-ssize_t set_search(int *set, size_t size, int v)
-{
+/*
+ *  MARK: set_search()
+ */
+ssize_t set_search(int *set, size_t size, int vh) {
 	int *ps;
+  ssize_t rv = -1;
 
-	for (ps = set; ps != set+size-1; ps++)
-		if (*ps == v)
-			return ps - set;
-	return -1;
+  printf("%12s: %p, %zu, %#04x\n",
+         __func__, set, size, vh);
+
+	for (ps = set; ps != set + size; ps++) {
+    printf("%12s: %#04x == %#04x : %-5s\n",
+           __func__, *ps, vh,
+           (*ps == vh) ? "true" : "false");
+		if (*ps == vh) {
+			rv = ps - set;
+    }
+  }
+	// rv = -1;
+  printf("%12s: ret = %ld\n",
+         __func__, rv);
+
+  return rv;
 }
